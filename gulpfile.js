342cf8d5bbea006ee4b2b27246313dd481ec67a3
Jeff Avallone <jeff.avallone@gmail.com>
2014-11-23 12:00:07 -0500

    Adding task to build pages wrapped in a template

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    wrap = require('gulp-wrap');
 
 gulp.task('default', function() {
   console.log('Default task');
+});
+
+gulp.task('markup', function() {
+  return gulp.src('./src/**/*.html')
+    .pipe(wrap({ src: './template.html' }))
+    .pipe(gulp.dest('./build'))
 });
