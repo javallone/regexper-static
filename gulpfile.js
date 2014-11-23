5a99aaa571a947fae306724beb4678d4a48b520e
Jeff Avallone <jeff.avallone@gmail.com>
2014-11-23 12:24:03 -0500

    Adding server with LiveReload support

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp'),
-    wrap = require('gulp-wrap');
+    wrap = require('gulp-wrap'),
+    connect = require('gulp-connect'),
+    watch = require('gulp-watch');
 
 gulp.task('default', function() {
   console.log('Default task');
+});
+
+gulp.task('server', function() {
+  watch('./build/**/*', { name: 'Server' })
+    .pipe(connect.reload());
+  return connect.server({
+    root: './build',
+    livereload: true
+  });
 });
 
 gulp.task('markup', function() {
