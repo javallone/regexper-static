c6f22718679214abef7b8a9cc644f49cea71cdd4
Jeff Avallone <jeff.avallone@gmail.com>
2014-11-25 20:05:29 -0500

    Adding a stubbed out test
    Removing old demo file and spec

---
+++ spec/parser/javascript_spec.js
@@ -0,0 +1,7 @@
+import parser from 'src/js/parser/javascript.peg';
+
+describe('parser/javascript.peg', function() {
+
+  pending();
+
+});
--- src/js/demo.js
+++
@@ -1,7 +0,0 @@
-export function test1() {
-  return 1;
-}
-
-export function test2() {
-  return 'two';
-}
