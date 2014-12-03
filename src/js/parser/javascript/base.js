9609b1d5b72566ed237a3faeaafa8bb17e26f149
Jeff Avallone <jeff.avallone@gmail.com>
2014-12-02 19:15:19 -0500

    Refactoring bounding box rendering method

--- src/js/parser/javascript/base.js
+++ src/js/parser/javascript/base.js
@@ -30,14 +30,10 @@ export default {
   },
 
   render_bbox(container, box) {
-    container.rect()
+    container.path(box.path)
       .attr({
-        'class': 'bounding-box',
-        width: box.width,
-        height: box.height
-      })
-      .transform(Snap.matrix()
-        .translate(box.x, box.y));
+        'class': 'bounding-box'
+      });
   },
 
   render(container) {
