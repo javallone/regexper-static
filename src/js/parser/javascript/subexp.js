ae064b2b8e6b809b94ff22618dfd8a3371f97a0d
Jeff Avallone <jeff.avallone@gmail.com>
2014-12-07 21:16:23 -0500

    Adding rendering for Subexp

--- src/js/parser/javascript/subexp.js
+++ src/js/parser/javascript/subexp.js
@@ -2,5 +2,67 @@ import _ from 'lodash';
 import Base from './base.js';
 
 export default _.extend({}, Base, {
-  type: 'subexp'
+  type: 'subexp',
+
+  labelMap: {
+    '?:': '',
+    '?=': 'positive lookahead',
+    '?!': 'negative lookahead'
+  },
+
+  render() {
+    var label = this.groupLabel();
+
+    if (label) {
+      this.container.addClass('subexp');
+
+      this.label = this.container.text()
+        .addClass('subexp-label')
+        .attr({
+          text: label
+        });
+
+      this.outline = this.container.rect()
+        .addClass('subexp-outline')
+        .attr({
+          rx: 3,
+          ry: 3
+        });
+
+      this.regexp.container = this.container.group().transform(Snap.matrix()
+        .translate(10, 10));
+      this.regexp.render();
+    } else {
+      this.regexp.container = this.container;
+      this.regexp.render();
+    }
+  },
+
+  position() {
+    var box;
+
+    this.regexp.position();
+
+    if (this.outline) {
+      box = this.regexp.container.getBBox();
+
+      this.outline.attr({
+        width: box.width + 20,
+        height: box.height + 20
+      });
+
+      this.container.path(Snap.format('M0,{content.cy}h10M{content.x2},{content.cy}H{container.x2}', {
+        container: this.container.getBBox(),
+        content: box
+      }));
+    }
+  },
+
+  groupLabel() {
+    if (_.has(this.labelMap, this._capture.textValue)) {
+      return this.labelMap[this._capture.textValue];
+    } else {
+      return 'group #TBA';
+    }
+  }
 });
