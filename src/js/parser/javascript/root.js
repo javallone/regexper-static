40ac83ab910b1a1b663ce6e422e1f77f72f33459
Jeff Avallone <jeff.avallone@gmail.com>
2014-12-14 19:28:50 -0500

    Revert "Simplifying positioning code for root nodes"
    This reverts commit 46ab0662f5ef8903ac15a44f71c56efbd9055796.

--- src/js/parser/javascript/root.js
+++ src/js/parser/javascript/root.js
@@ -13,9 +13,19 @@ export default _.extend({}, Base, {
       .attr({ r: 5 });
 
     return this.regexp.render(this.container.group())
-      .then(this.spaceHorizontally.bind(this, [this.start, this.regexp, this.end], {
-        padding: 10
-      }));
+      .then((() => {
+        var contentBox;
+
+        this.regexp.transform(Snap.matrix()
+          .translate(10, 0));
+
+        contentBox = this.regexp.getBBox();
+
+        this.start.transform(Snap.matrix()
+          .translate(0, contentBox.cy));
+        this.end.transform(Snap.matrix()
+          .translate(contentBox.x2 + 10, contentBox.cy));
+      }).bind(this));
   },
 
   flags() {
