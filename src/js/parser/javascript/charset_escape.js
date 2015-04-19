c22656a75d9ef9bfe67bb82bbfb3ec69127e4de4
Jeff Avallone <jeff.avallone@gmail.com>
2015-04-19 17:04:48 -0400

    Adding documentation to charset_escape.js

--- src/js/parser/javascript/charset_escape.js
+++ src/js/parser/javascript/charset_escape.js
@@ -1,3 +1,7 @@
+// CharsetEscape nodes are for escape sequences inside of character sets. They
+// differ from other [Escape](./escape.html) nodes in that `\b` matches a
+// backspace character instead of a word boundary.
+
 import _ from 'lodash';
 import Escape from './escape.js';
 
