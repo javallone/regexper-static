2377cb2497c381be32e5b0c48350db2d83329579
Jeff Avallone <jeff.avallone@gmail.com>
2018-02-10 17:27:30 -0500

    Removing unnecessary imports

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,7 @@ import React from 'react';
 
 import Message from './Message';
 
-import GithubIcon from 'feather-icons/dist/icons/github.svg';
-import DownloadIcon from 'feather-icons/dist/icons/download.svg';
-import LinkIcon from 'feather-icons/dist/icons/link.svg';
-import ChevronsDownIcon from 'feather-icons/dist/icons/chevrons-down.svg';
-import ErrorIcon from 'feather-icons/dist/icons/alert-octagon.svg';
-
-const App = () => <Message icon={ ErrorIcon } heading="React App">
+const App = () => <Message heading="React App">
   <p>Placeholder app content</p>
 </Message>;
 
