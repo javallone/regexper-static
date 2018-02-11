e0716ef683e7acea3a75af6c19af6eac27fc6f56
Jeff Avallone <jeff.avallone@gmail.com>
2018-02-11 12:13:15 -0500

    Adding tests for RavenBoundary

---
+++ src/components/RavenBoundary.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import RavenBoundary from './RavenBoundary';
+
+const testError = { error: 'test error' };
+const testDetails = { details: 'test details' };
+
+describe('RavenBoundary', () => {
+  test('rendering', () => {
+    const Child = () => <b>Child</b>;
+    const component = shallow(
+      <RavenBoundary>
+        <Child/>
+      </RavenBoundary>
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  test('rendering (with error)', () => {
+    const Child = () => <b>Child</b>;
+    const component = shallow(
+      <RavenBoundary>
+        <Child/>
+      </RavenBoundary>
+    );
+    expect(component).toMatchSnapshot();
+    component.instance().componentDidCatch(testError, testDetails);
+    component.update();
+    expect(component).toMatchSnapshot();
+  });
+});
