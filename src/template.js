eef5d504367658a8e40b5778279009d7270a0441
Jeff Avallone <jeff.avallone@gmail.com>
2018-02-11 07:09:03 -0500

    Tweaking some spacing

--- src/template.js
+++ src/template.js
@@ -45,10 +45,11 @@ module.exports = '<!DOCTYPE html>' + ReactDOMServer.renderToString(
             Created by <a href="mailto:jeff.avallone@gmail.com">Jeff Avallone</a>
           </li>
           <li>
-            Generated images licensed:
-            <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img
-              alt="Creative Commons CC-BY-3.0 License"
-              src="https://licensebuttons.net/l/by/3.0/80x15.png" /></a>
+            Generated images licensed: <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">
+              <img
+                alt="Creative Commons CC-BY-3.0 License"
+                src="https://licensebuttons.net/l/by/3.0/80x15.png" />
+            </a>
           </li>
         </ul>
       </footer>
