21146549f78361b2d999ffea7e20ba248f5d7a33
Jeff Avallone <jeff.avallone@gmail.com>
2018-02-11 18:43:11 -0500

    Adding language detection

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,12 @@
 import i18n from 'i18next';
 import { reactI18nextModule } from 'react-i18next';
+import LangDetector from 'i18next-browser-languagedetector';
 
 import locales from './locales';
 
 i18n
   .use(reactI18nextModule)
+  .use(LangDetector)
   .init({
     fallbackLng: 'en',
     debug: (process.env.NODE_ENV !== 'production'),
