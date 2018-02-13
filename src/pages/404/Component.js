5ab4a704142b0d3fbfcc79350a6750d426780867
Jeff Avallone <jeff.avallone@gmail.com>
2018-02-13 17:34:22 -0500

    Adjusting formatting on 404 page title and adding PropTypes

--- src/pages/404/Component.js
+++ src/pages/404/Component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { translate, Trans } from 'react-i18next';
 
 import Message from '../../components/Message';
@@ -6,7 +7,7 @@ import AlertIcon from 'feather-icons/dist/icons/alert-octagon.svg';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-const Component = ({ t }) => ( // eslint-disable-line react/prop-types
+const Component = ({ t }) => (
   <React.Fragment>
     <Header/>
     <Message type="error" icon={ AlertIcon } heading={ t('404 Page Not Found') }>
@@ -15,5 +16,9 @@ const Component = ({ t }) => ( // eslint-disable-line react/prop-types
     <Footer/>
   </React.Fragment>
 );
+
+Component.propTypes = {
+  t: PropTypes.func
+};
 
 export default translate()(Component);
