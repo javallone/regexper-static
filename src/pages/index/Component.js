99ecc123f4b435cc45858ea6edf077e059cbe685
Jeff Avallone <jeff.avallone@gmail.com>
2018-05-24 17:56:03 -0400

    Refering to the privacy policy from JS Disabled error message

--- src/pages/index/Component.js
+++ src/pages/index/Component.js
@@ -12,12 +12,7 @@ const Component = () => (
     <noscript>
       <Message type="error" icon={ AlertIcon } heading="JavaScript Required">
         <p>You need to enable JavaScript to use Regexper.</p>
-        <p>Regexper and the tools used to create it are all open source. If you are concerned that the JavaScript being delivered is in any way malicious, please inspect the source by following the GitHub link in the header. There are two data collection tools integrated in the app:</p>
-        <ul>
-          <li><b>Google Analytics</b> is used to track browser usage data and application performance.</li>
-          <li><b>Sentry.io</b> is a tool used to capture and report client-side JavaScript errors.</li>
-        </ul>
-        <p>Most popular ad blockers will prevent these tools from sending any tracking data, and doing so will <b>not</b> impact the performance of this app. Regexper is not supported by ad revenue or sales of any kind. The information collected by these tools is used to monitor application performance, determine browser support, and collect error reports.</p>
+        <p>If you have concerns regarding the use of tracking code on Regexper, please see the <a href="/privacy.html">Privacy Policy</a>.</p>
       </Message>
     </noscript>
     <App/>
